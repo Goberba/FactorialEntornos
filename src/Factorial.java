201cd07fdc03b2ad2fccf6afaf521d5af06b45f1
nada <nada>
2021-12-13 13:18:54 +0100

    Pues otro commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -11,7 +11,7 @@ public class Factorial {
 	}
 		public static void main(String[] args) {
 		
-		System.out.println("Dame un número:");
+		System.out.println("Dame un número:");asdas
 		long num = Console.readInt();
 		Factorial.hacerFactorial(num);
 	}
